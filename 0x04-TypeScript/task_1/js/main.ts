1c602d27a2d1a7172293638f240f2e439a6a2059
Eden Mengste <edenmengste@gmail.com>
2025-09-19 23:57:36 +0400

    Refactor printTeacher function to use function declaration

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -40,9 +40,8 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+function printTeacher(firstName: string, lastName: string): string {
+  return firstName.charAt(0) + ". " + lastName;
 
 console.log(printTeacher("John", "Doe"));  
 
