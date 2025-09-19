fceb1ca15b480a909a623b98588d1a635984c5f3
Eden Mengste <edenmengste@gmail.com>
2025-09-20 00:09:54 +0400

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -40,8 +40,8 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher( firstName: string, lastName: string): string {
-  return firstName.charAt(0) + ". " + lastName;
+function printTeacher( firstName, lastName ): string {
+   return `${firstName}. ${lastName}`;
 
 console.log(printTeacher("John", "Doe"));  
 
